c6c75907834e553b73a6d0f3372f2c05252aa3f0
nicholas mallare <nicholasmallare@gmail.com>
2022-04-03 20:00:46 -0500

    fix stuff

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,22 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+
+const API_URL = process.env.API_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+console.log(PRIVATE_KEY + "helllo");
+
 module.exports = {
   solidity: "0.7.3",
+  defaultNetwork: "ropsten",
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
  
